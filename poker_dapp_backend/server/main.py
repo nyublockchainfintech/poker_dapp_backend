a228da9b57e337c8dbd6c30a214b60d47ec317fd
AnishP15 <39468239+AnishP15@users.noreply.github.com>
2023-12-06 20:34:01 -0500

    feat: update profile and leave game

--- poker_dapp_backend/server/main.py
+++ poker_dapp_backend/server/main.py
@@ -6,31 +6,31 @@ import collections
 from pydantic import BaseModel
 from poker_dapp_backend.server.game import Game
 
-#from poker_dapp_backend.server.dealer import Dealer
+
+# poetry run python3 -m uvicorn poker_dapp_backend.server.main:app
+
 
 app = FastAPI()
 
 connected_players = set()
 
-# curr_id = 1
-
 rooms = collections.defaultdict(Game) #id:game
 
 rooms = {1:Game(buy_in=20, blinds=[20, 30]), 2:Game(buy_in=10, blinds=[20, 30])}
 
-class PlayerModel(BaseModel):
-    name: str
-    balance: int
-    hand: list
-    status: int 
     
-class JoinRoomModel(BaseModel):
+class PlayerRoomModel(BaseModel):
     game_id: int
-
     name: str
     balance: int
     hand: list
     status: int 
+
+class UpdateProfileModel(BaseModel):
+    game_id: int
+    old_name: str
+    new_name: str
+
 
 # TODO: Add join confirmation when a player joins the game
 # @app.websocket("/ws")
@@ -50,12 +50,11 @@ class JoinRoomModel(BaseModel):
 
 @app.get("/ws/rooms/{game_id}")
 async def get_game(game_id: int):
-    
     return {  rooms[game_id].serialize() }
 
  
 @app.get("/ws/all_rooms")
-async def get_game():
+async def get_games():
     curr_games = ""
 
     for game_id, _ in rooms.items():
@@ -63,6 +62,44 @@ async def get_game():
 
     return {  curr_games }
 
+@app.post("/ws/join_room")
+async def join_room(player: PlayerRoomModel):
+    # TODO CHECK REDIS IF NAME ALREADY EXISTS
+
+    if len(rooms[player.game_id].players) == rooms[player.game_id].max_players:
+        return { "Game is full, join another room" }
+
+    else:
+        rooms[player.game_id].add_player(player.name, player.balance)
+        return { rooms[player.game_id].serialize() }
+
+# TODO: Add game.remove()
+@app.post("/ws/leave_room")
+# async def leave_room(player: PlayerRoomModel):
+#     rooms[player.game_id].remove(player.name)
+
+#     return { rooms[player.game_id].serialize() } 
+
+@app.post("/ws/update_profile")
+async def update_profile(new_player: UpdateProfileModel):
+    # TODO CHECK REDIS IF OLD NAME ALREADY EXISTS
+
+    for player in rooms[new_player.game_id].players:
+        if player.name == new_player.old_name:
+            player.name = new_player.new_name
+
+    return { rooms[new_player.game_id].serialize() } 
+   
+
+# class PlayerModel(BaseModel):
+#     name: str
+#     balance: int
+#     hand: list
+#     status: int 
+
+# curr_id = 1
+
+# Function to add player to whatever the open room is 
 # @app.post("/ws/add_player")
 # async def add_player(player: PlayerModel):
 #     for game_id, game in rooms.items():
@@ -73,25 +110,6 @@ async def get_game():
 #             rooms[curr_id].add_player(player.name, player.balance)
 #             curr_id += 1
 #     return { player }
-
-@app.post("/ws/join_room")
-async def join_room(player: JoinRoomModel):
-
-    if len(rooms[player.game_id].players) == rooms[player.game_id].max_players:
-        return { "Game is full, join another room" }
-
-    else:
-        rooms[player.game_id].add_player(player.name, player.balance)
-        return { rooms[player.game_id].serialize() }
-
-
-
-
-
-
-
-
-
 
 
 
--- poker_dapp_backend/server/test_server.sh
+++ poker_dapp_backend/server/test_server.sh
@@ -1,1 +1,10 @@
-curl -X POST -H "Content-Type: application/json" -d '{"game_id": 1, "name": "John Doe", "balance": 100, "hand": [], "status": 0}' http://127.0.0.1:8000/ws/join_room
+
+# testing join_room
+curl -X POST -H "Content-Type: application/json" -d '{"game_id": 1, "name": "Noah", "balance": 100, "hand": [], "status": 0}' http://127.0.0.1:8000/ws/join_room
+
+# testing update profile
+curl -X POST -H "Content-Type: application/json" -d '{"game_id": 1, "old_name": "Noah", "new_name": "Anish"}' http://127.0.0.1:8000/ws/update_profile
+
+
+# testing leave_room
+# curl -X POST -H "Content-Type: application/json" -d '{"game_id": 1, "name": "Anish", "balance": 100, "hand": [], "status": 0}' http://127.0.0.1:8000/ws/leave_room
