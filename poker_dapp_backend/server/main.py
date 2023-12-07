2de1af5bcb0eb36d5252589ae1899d4a102360ac
Noah Perelmuter <np2446@stern.nyu.edu>
2023-12-07 13:19:28 -0500

    added sample messages, fixed messaging

--- poker_dapp_backend/server/main.py
+++ poker_dapp_backend/server/main.py
@@ -33,48 +33,53 @@ async def websocket_endpoint(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_json()
+            msg_type = data.get("MESSAGE_TYPE", None)
             msg = data.get("MESSAGE", None)
             game_id = msg.get("GAME_ID", None)
-            msg_type = msg.get("MESSAGE TYPE", None)
-
+            
             # Process different message types
-            if msg_type == "BET":
+            if msg_type == "ACTION":
                 player_index = rooms[game_id].get_player_index(msg["PLAYER NAME"])
-                rooms[game_id].player_bet(player_index, data["AMOUNT"])
+                match msg["ACTION"]:
+                    case "BET":
+                        rooms[game_id].player_bet(player_index, msg["AMOUNT"])
 
-            elif msg_type == "CHECK":
-                player_index = rooms[game_id].get_player_index(msg["PLAYER NAME"])
-                rooms[game_id].player_check(player_index, data["AMOUNT"])
+                    case "CHECK":
+                        rooms[game_id].player_check(player_index)
 
-            elif msg_type == "FOLD":
-                player_index = rooms[game_id].get_player_index(msg["PLAYER NAME"])
-                rooms[game_id].player_fold(player_index)
+                    case "FOLD":
+                        rooms[game_id].player_fold(player_index)
+
+                    case _: 
+                        raise ValueError("Invalid action")
 
             elif msg_type == "START_GAME":
                 rooms[game_id].start_game()
 
             elif msg_type == "JOIN":
                 rooms[game_id].add_player(
-                    msg["PLAYER NAME"],
-                    msg["PLAYER BALANCE"],
+                    msg["PLAYER_NAME"],
+                    msg["PLAYER_BALANCE"],
                 )
                 room_sockets[game_id].append(websocket)
 
             elif msg_type == "LEAVE":
-                rooms[game_id].remove_player(msg["PLAYER NAME"])
+                rooms[game_id].remove_player(msg["PLAYER_NAME"])
                 room_sockets[game_id].remove(websocket)
 
             elif msg_type == "CREATE":
                 game_id = room_counter
+                small_blind = int(msg["BLINDS"][0])
+                big_blind = int(msg["BLINDS"][1])
+                blinds = (small_blind, big_blind)
                 rooms[game_id] = Game(
                     buy_in=msg["BUY_IN"],
-                    blinds=msg["BLINDS"],
+                    blinds=blinds,
                 )
                 room_counter += 1
                 room_sockets[game_id] = []
 
             # Broadcast received data to all connected clients
-            
             new_game_state = json.loads(rooms[game_id].serialize())
             for ws in room_sockets[game_id]:
                 await ws.send_json(new_game_state)  # Modify as needed for broadcasting
@@ -82,3 +87,4 @@ async def websocket_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         connected_players.remove(websocket)
         print(f"Client {websocket} disconnected")
+
