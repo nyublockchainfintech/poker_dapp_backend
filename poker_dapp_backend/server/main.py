0901d21f45fb848f6e61b72d85e1312e5dd4dfa0
Noah Perelmuter <np2446@stern.nyu.edu>
2023-12-06 21:51:46 -0500

    added ws listening

--- poker_dapp_backend/server/main.py
+++ poker_dapp_backend/server/main.py
@@ -16,7 +16,7 @@ curr_id = 1
 
 rooms = collections.defaultdict(Game) #id:game
 
-rooms = {1:Game(buy_in=20, blinds=[20, 30]), 2:Game(buy_in=10, blinds=[20, 30])}
+# rooms = {1:Game(buy_in=20, blinds=[20, 30]), 2:Game(buy_in=10, blinds=[20, 30])}
 
 class PlayerModel(BaseModel):
     name: str
@@ -24,18 +24,28 @@ class PlayerModel(BaseModel):
     hand: list
     status: int 
     
-#TODO: Add join confirmation when a player joins the game
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     connected_players.add(websocket)
-    dealer = Dealer(connected_players)
     try:
         while True:
             data = await websocket.receive_json()
-            print("Data received from a client:", data)
+            game_id = data["MESSAGE"]["GAME ID"]
+            # Process different message types
+            if data["MESSAGE TYPE"] == "BET":
+                rooms[game_id].bet(data["PLAYER ID"], data["AMOUNT"])
+
+            elif data["MESSAGE TYPE"] == "JOIN":
+                rooms[game_id].add_player(data["MESSAGE"]["PLAYER NAME"], data["MESSAGE"]["PLAYER BALANCE"])
+
+            elif data["MESSAGE TYPE"] == "CREATE":
+                rooms[game_id] = Game(buy_in=data["MESSAGE"]["BUY IN"], blinds=data["MESSAGE"]["BLINDS"])
+
             # Broadcast received data to all connected clients
-            await dealer.reply(data)
+            for ws in connected_players:
+                await ws.send_json(data)  # Modify as needed for broadcasting
+
     except WebSocketDisconnect:
         connected_players.remove(websocket)
         print(f"Client {websocket} disconnected")
