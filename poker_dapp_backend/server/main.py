b764ca12483e11b40dbbbc08a9d5ada1cd9dc258
Vishakh Sandwar <vishakhs74@gmail.com>
2023-12-06 23:55:57 -0500

    fix: cors header issue

--- poker_dapp_backend/server/main.py
+++ poker_dapp_backend/server/main.py
@@ -4,13 +4,25 @@ from poker_dapp_backend.server.game import Game
 from poker_dapp_backend.server.player import Player
 import collections
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 #from poker_dapp_backend.server.dealer import Dealer
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:3000", # ADD YOUR FRONTEND URL HERE
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 connected_players = set()
-curr_games = ""
 
 curr_id = 1
 
@@ -45,24 +57,25 @@ async def get_game(game_id: int):
     return {  rooms[game_id].serialize() }
 
  
-# @app.get("/ws/all_rooms")
-# async def get_game():
+@app.get("/ws/all_rooms")
+async def get_game():
+    curr_games = ""
 
-#     for game_id, _ in rooms.items():
-#         curr_games += rooms[game_id].serialize()
+    for game_id, _ in rooms.items():
+        curr_games += rooms[game_id].serialize()
 
-#     return {  curr_games }
+    return {  curr_games }
 
-# @app.post("/ws/add_player")
-# async def add_player(player: PlayerModel):
-#     for game_id, game in rooms.items():
-#         if game.add_player(player.name, player.balance):
-#             rooms[game_id] = game
-#         else: 
-#             rooms[curr_id] = Game()
-#             rooms[curr_id].add_player(player.name, player.balance)
-#             curr_id += 1
-#     return player
+@app.post("/ws/add_player")
+async def add_player(player: PlayerModel):
+    for game_id, game in rooms.items():
+        if game.add_player(player.name, player.balance):
+            rooms[game_id] = game
+        else: 
+            rooms[curr_id] = Game()
+            rooms[curr_id].add_player(player.name, player.balance)
+            curr_id += 1
+    return player
 
 
 
